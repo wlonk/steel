898447fd5d08f9dae6d677400561de939b9fb011
Kit La Touche <kit@transneptune.net>
2015-09-18 00:00:37 -0600

    Crude implementation of first three endpoints.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,33 @@
+#[macro_use]
+extern crate router;
 extern crate iron;
+extern crate urlencoded;
 
 use iron::prelude::*;
 use iron::status;
+use urlencoded::UrlEncodedBody;
+
+#[allow(unused_variables)]
+fn index(req: &mut Request) -> IronResult<Response> {
+    let payload = "this page";
+    Ok(Response::with((status::Ok, payload)))
+}
+
+fn ip_handler(req: &mut Request) -> IronResult<Response> {
+    Ok(Response::with((status::Ok, req.remote_addr.to_string())))
+}
+
+fn post_handler(req: &mut Request) -> IronResult<Response> {
+    let payload = match req.get_ref::<UrlEncodedBody>() {
+        Ok(ref hashmap) => format!("{:?}", hashmap),
+        Err(ref e) => format!("{:?}", e),
+    };
+    Ok(Response::with((status::Ok, payload)))
+}
 
 fn main() {
-    Iron::new(|req: &mut Request| {
-        Ok(Response::with((status::Ok, "Hello World!")))
-    }).http("localhost:3000").unwrap();
+    let router = router!(get  "/"       => index,
+                         get  "/ip"     => ip_handler,
+                         post "/post"   => post_handler);
+    Iron::new(router).http("localhost:3000").unwrap();
 }
